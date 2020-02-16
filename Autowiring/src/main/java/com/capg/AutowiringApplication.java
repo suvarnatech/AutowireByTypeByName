0fc6a9ec32f90e19f7c059f1d380e3fa31f5eabb
sukan <sukan@192.168.40.28>
2020-02-16 11:26:22 +0530

    Autowire byType and ByName

---
+++ Autowiring/src/main/java/com/capg/AutowiringApplication.java
@@ -0,0 +1,18 @@
+package com.capg;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.capg.model.Employee;
+
+@SpringBootApplication
+public class AutowiringApplication {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Employee e = (Employee) context.getBean("emp");
+		System.out.println(e);
+
+	}
+
+}
